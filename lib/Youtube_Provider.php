79134ac132a01bc743fc7f6c15cc3d9955397050
Nick Freear <nfreear@yahoo.co.uk>
2015-07-09 12:11:45 +0100

    Bug #7, 'Local' embed of YouTube, with "oup-light" (non-YouTube) theme [iet:3634892]

--- lib/Youtube_Provider.php
+++ lib/Youtube_Provider.php
@@ -6,10 +6,17 @@
  * @copyright Copyright 2012 The Open University.
  */
 
-
 use \IET_OU\Open_Media_Player\Oembed_Provider;
+use \IET_OU\Open_Media_Player\Oembed_Local_Embed_Interface;
 
-class Youtube_Provider extends Oembed_Provider
+
+// TODO: move to separate file!
+class Youtube_Player extends \IET_OU\Open_Media_Player\Base_Player
+{
+}
+
+
+class Youtube_Provider extends Oembed_Provider implements Oembed_Local_Embed_Interface
 {
 
     public $regex = array('http://*youtube.com/watch*', 'http://youtu.be/*');
@@ -18,15 +25,16 @@ class Youtube_Provider extends Oembed_Provider
   Embed videos from YouTube with a HTML5-video option. [Initially for Cloudworks/OULDI. Public access.]
 EOT;
     public $displayname = 'YouTube';
-  #public $name = 'youtube';
+    #public $name = 'youtube';
     public $domain = 'youtube.com';
     public $subdomains = array('m.youtube.com');
     public $favicon = 'http://youtube.com/favicon.ico';
     public $type = 'video';
 
     public $_about_url = 'http://youtube.com/';
-    public $_regex_real = '(youtu\.be\/|youtube\.com\/watch\?.*v=)([\w-_]+)&*.*';
+    public $_regex_real = '(youtu\.be\/|youtube\.com\/watch\?.*v=)(?P<id>[\w\-_]+)&*.*';
     public $_examples = array(
+    'Vice-Chancellor Peter Horrocks' => 'http://youtu.be/EQEy5_QE2tQ',
     'Interview with Martin Bean (captions)' => 'http://youtube.com/watch?v=NaBBk-kpmL4',
     'http://youtu.be/NaBBk-kpmL4',
     'Brian McAllister, Roadtrip Nation (OLnet)' => 'http://youtube.com/watch?v=VesKht_8HCo',
@@ -34,16 +42,29 @@ EOT;
     );
     public $_access = 'public';
 
+    const API_URL =
+    'https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails,id,player,status,contentDetails,snippet&id=%s&key=%s';
 
     /**
-  * Implementation of call().
-  * @return object
-  */
+    * Implementation of call().
+    * @return object
+    */
     public function call($url, $matches)
     {
-        $video_id = $matches[2]; #1
+        $video_id = $matches[ 'id' ]; #1
+        $theme = $this->param_get('theme');
+        if ('oup-light' == $theme) {
+            $embed_url = site_url(sprintf('/embed/-/%s/%s', $his->domain, $video_id));
+        }
+        elseif (preg_match('/^(dark|light)$', $theme)) {
+            $embed_url = sprintf('//www.youtube.com/embed/%s', $video_id);
+        } else {
+            $this->_error("Unrecognized value for YouTube parameter {theme}", 400);
+        }
 
         $meta = array(
+            'embed_url' => $embed_url,
+            'theme' => $theme,
         'url'=>$url,
         'provider_name'=> 'YouTube',
         'provider_mid' => $video_id,
@@ -57,5 +78,79 @@ EOT;
         );
 
         return (object) $meta;
+    }
+
+
+    protected function api_request($video_id)
+    {
+        // Create a project & get a server API key ...
+        // https://console.developers.google.com/project
+        $api_key = $this->config_item('youtube_api_key');
+
+        $api_url = sprintf(self::API_URL, $video_id, $api_key);
+
+        $http = new \IET_OU\Open_Media_Player\Http();
+        $resp = $http->request($api_url);
+        /*if ($resp->is_not_found) {
+            $this->_error("YouTube video not found, $video_id", 404);
+        }
+        else*/if (!$resp->success) {
+            $this->_error("YouTube embed error", $resp->http_code);
+        }
+        $resp->obj = json_decode($resp->data);
+        return $resp;
+    }
+
+    public function local_embed($video_id)
+    {
+        $resp = $this->api_request($video_id);
+
+        $this->_debug($resp->obj);
+
+        if (1 !== $resp->obj->pageInfo->totalResults) {
+            $this->_error("YouTube video not found, $video_id", 404);
+        }
+
+        $video = $resp->obj->items[ 0 ];
+        $thumbnail = $video->snippet->thumbnails->high;
+
+        $_debug = true;
+        $_theme_name = 'oup-light';
+
+        $player = new \IET_OU\Open_Oembed_Providers\Youtube_Player([
+            'id' => $video_id,
+            'title' => $video->snippet->title,
+            'mime_type' => 'video/youtube',
+            'media_type' => 'video',
+            'media_url' => 'https://youtube.com/watch?v=' . $video_id,
+            'poster_url' => $thumbnail->url,
+            'width'  => $thumbnail->width,
+            'height' => $thumbnail->height,
+            'iso_duration' => $video->contentDetails->duration,
+            '_theme' => $_theme_name,
+        ]);
+
+        $view_data = array(
+            'meta' => $player,
+            'theme'=> $_theme_name,
+            'debug'=> $_debug,
+            'standalone' => false,
+            // Auto-generate 'embed' or 'popup'.
+            'mode' => strtolower(get_class($this)),
+            'req'  => null,
+            'google_analytics' => null,  //TODO.
+            'popup_url' => null,
+            '_caption_url' => null,
+        );
+
+        $this->theme = $this->CI->load->theme($_theme_name);
+
+        $this->theme->prepare($player);
+
+        $view_data['params'] = $view_data['meta'];
+        $view_data['params']->debug = $this->CI->_is_debug(OUP_DEBUG_MAX);  #$this->_debug;
+        $view_data['params']->debug_score = $this->CI->_is_debug(1, $score = true);
+
+        $this->CI->load->theme_view(null, $view_data);
     }
 }
